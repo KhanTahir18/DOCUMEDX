765a5a8eef3a140b7a2dcfbe543ac64de7c26994
Tahir Khan <samuel16907@gmail.com>
2025-07-23 17:11:26 +0530

    -Added Functionality to the sign up button

--- app/src/main/java/com/example/documedx/MainActivity.kt
+++ app/src/main/java/com/example/documedx/MainActivity.kt
@@ -7,14 +7,30 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.documedx.databinding.ActivityMainBinding
 import com.example.documedx.databinding.BasicHealthInfoGatheringActivityBinding
+import com.example.documedx.databinding.SignUpPageActiviyBinding
 
 class MainActivity : AppCompatActivity() {
+    //Bind For Sign up Page
+    private lateinit var bindingSignUpPage: SignUpPageActiviyBinding
 
-    private lateinit var binding: ActivityMainBinding
+    //Bind For Info Gathering Page
+    private lateinit var bindingBasicHEalthInfoGatheringPage: BasicHealthInfoGatheringActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        //Setting the Sign up Page as default
+        bindingSignUpPage = SignUpPageActiviyBinding.inflate(layoutInflater)
+        setContentView(bindingSignUpPage.root)
+
+        //Sign up Button OnclickListener
+        bindingSignUpPage.signUpBtn.setOnClickListener {
+
+            //Setting info gathering page as default
+            bindingBasicHEalthInfoGatheringPage = BasicHealthInfoGatheringActivityBinding.inflate(layoutInflater)
+            setContentView(bindingBasicHEalthInfoGatheringPage.root)
+        }
     }
+
+
 }
