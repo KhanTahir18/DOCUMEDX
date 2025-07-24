3deeaf745de11c64838a22b3d7e1b52985967929
Tahir Khan <samuel16907@gmail.com>
2025-07-24 19:15:55 +0530

    Correct The wrong code sent by Batman

---
+++ app/src/main/java/com/example/documedx/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.documedx
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.documedx.databinding.ActivityIntroBinding
+import com.google.firebase.Firebase
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class IntroActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.staffBtn.setOnClickListener {
+            val intent = Intent(this, StaffSignUpActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
