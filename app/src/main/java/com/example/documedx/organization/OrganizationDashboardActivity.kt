22ae211c4fec8ff244de23ccaf24dcfb8d6d27c3
Tahir Khan <samuel16907@gmail.com>
2025-08-05 13:02:20 +0530

    syncing files

--- app/src/main/java/com/example/documedx/organization/OrganizationDashboardActivity.kt
+++ app/src/main/java/com/example/documedx/organization/OrganizationDashboardActivity.kt
@@ -14,11 +14,11 @@ class OrganizationDashboardActivity: AppCompatActivity() {
         setContentView(binding.root)
 
         val licence = intent.getStringExtra("licence")
-        binding.viewMoreBtnStaff.setOnClickListener {
-            val intent = Intent(this, OrganizationDepartmentActivity::class.java)
-            intent.putExtra("licence", licence)
-            startActivity(intent)
-        }
+//        binding.viewMoreBtnStaff.setOnClickListener {
+//            val intent = Intent(this, OrganizationDepartmentActivity::class.java)
+//            intent.putExtra("licence", licence)
+//            startActivity(intent)
+//        }
 
         onBackPressedDispatcher.addCallback(this) {
             finishAffinity() // Or whatever you want to do on back press
